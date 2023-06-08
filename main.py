969e218c745689b42ffb38535bcc47833043d08f
Lumine <lumine.mona@outlook.com>
2023-06-08 19:00:28 +0300

    Update readme

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from mizue.printer import Printer, TerminalColors
 
 
-def print_hi(name):
-    Printer.error(f'Hi, {name}')
-    Printer.print(f'Hi, {name}', TerminalColors.YELLOW)
+def print_message(name):
+    Printer.prevent_newline(True)
+    Printer.error(f'Hi, {name}. ')
+    Printer.print(f'How are you?', TerminalColors.YELLOW)
+    Printer.prevent_newline(False)
+    Printer.prevent_newline(False)
+    Printer.prevent_newline(False)
+    Printer.prevent_newline(False)
+    Printer.prevent_newline(False)
+    Printer.print('This is a test', TerminalColors.RED, True, True)
+    Printer.print('This is a test', TerminalColors.BG_CYAN, True, True)
 
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Mizue')
+    print_message('Mizu')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- mizue/printer/printer.py
+++ mizue/printer/printer.py
@@ -11,6 +11,7 @@ class Printer:
 
     @staticmethod
     def format(text: str, color: str, bold: bool = False, underlined: bool = False, no_end: bool = False) -> str:
+        """Formats a string with the specified color, boldness, and underlining."""
         # call('', shell=True)  # Enable VT100 mode
         bolded = TerminalColors.BOLD if bold else ''
         underlined = TerminalColors.UNDERLINE if underlined else ''
@@ -19,25 +20,35 @@ class Printer:
 
     @staticmethod
     def error(text: str, bold: bool = False, underlined: bool = False) -> None:
+        """Prints an error message to the console."""
         Printer.print(Printer.format(text, TerminalColors.ERROR, bold, underlined))
 
     @staticmethod
     def info(text: str, bold: bool = False, underlined: bool = False) -> None:
+        """Prints an info message to the console."""
         Printer.print(Printer.format(text, TerminalColors.INFO, bold, underlined))
 
     @staticmethod
     def prevent_newline(prevent: bool = True) -> None:
+        """Prevents a newline from being printed to the console."""
+        if Printer._newline != prevent:
+            return
         Printer._newline = not prevent
+        if Printer._newline:
+            print()
 
     @staticmethod
     def print(text: str, color: str = TerminalColors.WHITE, bold: bool = False, underlined: bool = False) -> None:
+        """Prints a message to the console."""
         formatted_text = text if Printer._formatted(text) else Printer.format(text, color, bold, underlined)
         print(formatted_text, end='\n' if Printer._newline else '', flush=True)
 
     @staticmethod
     def success(text: str, bold: bool = False, underlined: bool = False) -> None:
+        """Prints a success message to the console."""
         Printer.print(Printer.format(text, TerminalColors.SUCCESS, bold, underlined))
 
     @staticmethod
     def warning(text: str, bold: bool = False, underlined: bool = False) -> None:
+        """Prints a warning message to the console."""
         Printer.print(Printer.format(text, TerminalColors.WARNING, bold, underlined))
