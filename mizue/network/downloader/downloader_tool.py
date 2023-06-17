6e26d403261c1eedcd1cf746b755a4394184d233
Lumine <lumine.mona@outlook.com>
2023-06-18 02:22:02 +0300

    Add a method for bulk downloading [url, path] tuples

--- mizue/network/downloader/downloader_tool.py
+++ mizue/network/downloader/downloader_tool.py
@@ -13,7 +13,7 @@ from mizue.progress import Progress, ProgressBarRendererArgs, SpinnerRendererArg
 
 
 @dataclass
-class DownloadReport:
+class _DownloadReport:
     filename: str
     filesize: int
     url: str
@@ -21,13 +21,19 @@ class DownloadReport:
 
 class DownloaderTool:
     def __init__(self):
-        self._report_data: list[DownloadReport] = []
+        self._report_data: list[_DownloadReport] = []
         self._bulk_download_size = 0
         self._downloaded_count = 0
         self._total_download_count = 0
         self.progress: Progress | None = None
 
     def download(self, url: str, output_path: str):
+        """
+        Download a file to a specified directory
+        :param url: The URL to download
+        :param output_path: The output directory
+        :return: None
+        """
         filepath = []
         downloader = Downloader()
         downloader.add_event(DownloadEventType.STARTED, lambda event: self._on_download_start(event, filepath))
@@ -42,10 +48,46 @@ class DownloaderTool:
             Printer.warning(f"{os.linesep}Keyboard interrupt detected. Cleaning up...")
             if len(filepath) > 0:
                 os.remove(filepath[0])
-            self._report_data.append(DownloadReport(url, 0, url))
+            self._report_data.append(_DownloadReport(url, 0, url))
         self._print_report()
 
-    def download_bulk(self, urls: list[str], output_path: str, parallel: int = 4):
+    def download_bulk(self, urls: list[str] | list[tuple[str, str]], output_path: str | None = None, parallel: int = 4):
+        """
+        Download a list of files to a specified directory or a list of [url, output_path] tuples.
+
+        If the urls parameter is a list of [url, output_path] tuples, every url will be downloaded to its corresponding
+        output_path.
+
+        If the urls parameter is a list of urls, every url will be downloaded to the output_path parameter. In this case,
+        the output_path parameter must be specified.
+        :param urls: A list of urls or a list of [url, output_path] tuples
+        :param output_path: The output directory if the urls parameter is a list of urls
+        :param parallel: Number of parallel downloads
+        :return: None
+        """
+        if isinstance(urls[0], tuple):
+            self.download_tuple(urls, parallel)
+        else:
+            self.download_list(urls, output_path, parallel)
+
+    def download_list(self, urls: list[str], output_path: str, parallel: int = 4):
+        """
+        Download a list of files to a specified directory
+        :param urls: The list of URLs to download
+        :param output_path: The output directory
+        :param parallel: Number of parallel downloads
+        :return: None
+        """
+        self.download_tuple([(url, output_path) for url in urls], parallel)
+
+    def download_tuple(self, urls: list[tuple[str, str]], parallel: int = 4):
+        """
+        Download a list of [url, output_path] tuples. Every url will be downloaded to its corresponding output_path.
+        :param urls: A list of [url, output_path] tuples
+        :param parallel: Number of parallel downloads
+        :return: None
+        """
+
         self.progress = Progress(start=0, end=len(urls), value=0)
         self._configure_progress()
         self.progress.start()
@@ -62,7 +104,7 @@ class DownloaderTool:
                 downloader.add_event(DownloadEventType.COMPLETED,
                                         lambda event: self._on_bulk_download_complete(event))
                 downloader.add_event(DownloadEventType.FAILED, lambda event: self._on_bulk_download_failed(event))
-                for url in list(set(urls)):
+                for url, output_path in list(set(urls)):
                     responses.append(executor.submit(downloader.download, url, output_path))
                 for response in concurrent.futures.as_completed(responses):
                     self._downloaded_count += 1
@@ -116,10 +158,10 @@ class DownloaderTool:
         return Printer.format_hex(args.label, '#FFCC75')
 
     def _on_bulk_download_complete(self, event: DownloadCompleteEvent):
-        self._report_data.append(DownloadReport(event.filename, event.filesize, event.url))
+        self._report_data.append(_DownloadReport(event.filename, event.filesize, event.url))
 
     def _on_bulk_download_failed(self, event: DownloadFailureEvent):
-        self._report_data.append(DownloadReport("", 0, event.url))
+        self._report_data.append(_DownloadReport("", 0, event.url))
 
     def _on_bulk_download_progress(self, event: ProgressEventArgs, download_dict: dict):
         download_dict[event.url] = event.downloaded
@@ -133,7 +175,7 @@ class DownloaderTool:
         self.progress.set_info(info)
         time.sleep(0.5)
         self.progress.stop()
-        self._report_data.append(DownloadReport(event.filename, event.filesize, event.url))
+        self._report_data.append(_DownloadReport(event.filename, event.filesize, event.url))
 
     def _on_download_failure(self, event: DownloadFailureEvent):
         if isinstance(event.exception, KeyboardInterrupt):
@@ -141,7 +183,7 @@ class DownloaderTool:
             print(os.linesep)
         if self.progress:
             self.progress.terminate()
-        self._report_data.append(DownloadReport("", 0, event.url))
+        self._report_data.append(_DownloadReport("", 0, event.url))
 
     def _on_download_progress(self, event: ProgressEventArgs):
         self.progress.update_value(event.downloaded)
