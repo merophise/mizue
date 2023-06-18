ae73c78ffce0b9af369f412f6937ffd0252254e2
Lumine <lumine.mona@outlook.com>
2023-06-18 04:30:14 +0300

    Add initial CI/CD config

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md")) as f:
 
 setup(
     name="mizue",
-    version="0.2.0",
+    version="0.2.1",
     description="A Python package for various utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
