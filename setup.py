d9365a7c459f91222fb76208b59cd081b6a19cdd
GitHub Action <action@github.com>
2023-06-19 00:21:49 +0000

    Bump version to 0.2.15

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md")) as f:
 
 setup(
     name="mizue",
-    version="0.2.14",
+    version="0.2.15",
     description="A Python package for various utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
